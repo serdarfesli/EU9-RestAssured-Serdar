fd8ec4467dafc565bcc74ac9270d84b78df0f0e4
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-11-07 19:44:10 +0300

    second automation case done

--- src/test/java/com/cydeo/day2/SpartansGetRequest.java
+++ src/test/java/com/cydeo/day2/SpartansGetRequest.java
@@ -29,6 +29,51 @@ public class SpartansGetRequest {
       Assertions.assertEquals("application/json",response.contentType());
       response.prettyPrint();
 
+   }
+
+   @Test
+   public void test2(){
+       /*
+        Given accept header is application/json
+        When users sends a get request to /api/spartans/3
+        Then status code should be 200
+        And content type should be application/json
+        and json body should contain Fidole
+     */
+
+      Response response = RestAssured.given().accept(ContentType.JSON)
+              .when().get(url + "/api/spartans/3");
+
+      System.out.println("response.statusCode() = " + response.statusCode());
+      System.out.println("response.contentType() = " + response.contentType());
+      Assertions.assertEquals(200,response.statusCode());
+      Assertions.assertEquals("application/json",response.contentType());
+      Assertions.assertTrue(response.body().asString().contains("Fidole"));
+   }
+
+   /*
+        Given no headers provided
+        When Users sends GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be “text/plain;charset=UTF-8”
+        And header should contain date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+   @Test
+   public void test3(){
+      Response response = RestAssured.when().get(url + "/api/hello");
+
+      Assertions.assertEquals(200,response.statusCode());
+      Assertions.assertEquals("text/plain;charset=UTF-8",response.contentType());
+      Assertions.assertEquals("text/plain;charset=UTF-8",response.contentType());
+      Assertions.assertTrue(response.headers().hasHeaderWithName("date"));
+      Assertions.assertTrue(response.header("Content-Length").equals("17"));
+      Assertions.assertTrue(response.body().asString().equals("Hello from Sparta"));
+
+
+
+
 
    }
 }
