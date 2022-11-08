6a860b3f884eaa9a2abb761d816df6b7fedd879a
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-11-08 09:21:00 +0300

    pathparameter test1 done

--- src/test/java/com/cydeo/day3/SpartanTestWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestWithParameters.java
@@ -1,5 +1,33 @@
 package com.cydeo.day3;
 
-public class SpartanTestWithParameters {
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+
+public class SpartanTestWithParameters {
+   @BeforeAll
+   public static void init() {
+      baseURI = "http://44.208.34.43:8000";
+   }
+   /*   Given accept type is Json
+             And Id parameter value is 5
+             When user sends GET request to /api/spartans/{id}
+             Then response status code should be 200
+             And response content-type: application/json
+             And "Blythe" should be in response payload
+          */
+   @Test
+   public void test1(){
+      Response response = given().pathParam("id", 5)
+              .when().get("/api/spartans/{id}");
+
+      assertEquals(200,response.statusCode());
+      assertEquals("application/json",response.contentType());
+      assertTrue(response.body().asString().contains("Blythe"));
+
+   }
 }
