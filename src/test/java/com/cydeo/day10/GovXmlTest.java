c2fae2f1e99223abbcd68e6677a491a1ff9ac568
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-12-10 10:08:56 +0300

    json schema validator dependency added

--- src/test/java/com/cydeo/day10/GovXmlTest.java
+++ src/test/java/com/cydeo/day10/GovXmlTest.java
@@ -1,5 +1,22 @@
 package com.cydeo.day10;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 public class GovXmlTest {
+   @Test
+   public void test1() {
+      //send a get request to
+      //https://data.ct.gov/api/views/qm34-pq7e/rows.xml
+      //get all the years
+      //get all unknowns
+      //get 2005 other
+      //get 2007 _address
 
+      XmlPath xmlPath = given().accept(ContentType.XML).when().get("https://data.ct.gov/api/views/qm34-pq7e/rows.xml").then().extract().xmlPath();
+
+   }
 }
---
+++ src/test/java/com/cydeo/day10/JsonSchemaValidatonTest.java
@@ -0,0 +1,14 @@
+package com.cydeo.day10;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JsonSchemaValidatonTest extends SpartanAuthTestBase {
+
+   @DisplayName("GET request to verify one spartan against to schema")
+   @Test
+   public void schemaValidation(){
+
+   }
+}
