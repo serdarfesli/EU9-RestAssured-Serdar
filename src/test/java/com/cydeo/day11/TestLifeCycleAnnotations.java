0a27e89e8824b8ff022539fe9e6c7a8efe27cb82
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-12-11 09:55:03 +0300

    test annotations added

--- src/test/java/com/cydeo/day11/TestLifeCycleAnnotations.java
+++ src/test/java/com/cydeo/day11/TestLifeCycleAnnotations.java
@@ -1,4 +1,35 @@
 package com.cydeo.day11;
 
+import org.junit.jupiter.api.*;
+
 public class TestLifeCycleAnnotations {
+
+   @BeforeAll
+   public static void init(){
+      System.out.println("Before All is running");
+   }
+
+   @BeforeEach
+   public void initEach(){
+      System.out.println("Before each is running");
+   }
+
+   @Test
+   public void test1(){
+      System.out.println("test 1 is running");
+   }
+
+   @AfterEach
+   public void initEach2(){
+      System.out.println("After each is running");
+   }
+   @Test
+   public void test2(){
+      System.out.println("test 2 is running");
+   }
+
+   @AfterAll
+   public static void close(){
+      System.out.println("After all is running");
+   }
 }
