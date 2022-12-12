c33a37d5224a228d1f4e1df1106a261fff97018b
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-12-12 16:44:47 +0300

    old rest version tried

--- src/test/java/com/cydeo/day12/OldRestAssuredTest.java
+++ src/test/java/com/cydeo/day12/OldRestAssuredTest.java
@@ -1,14 +1,22 @@
 package com.cydeo.day12;
 
 import com.cydeo.utilities.SpartanNewBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 public class OldRestAssuredTest extends SpartanNewBase {
-    @Test
+    @Test //this is current verison
     public void getAllSpartan(){
-
+    given()
+            .accept(ContentType.JSON)
+            .auth().basic("admin","admin")
+            .log().all()
+            .when().get("/spartans")
+            .then().statusCode(200)
+            .body("id[0]",is(10))
+            .body("id[5]",is(19));
     }
 
 
