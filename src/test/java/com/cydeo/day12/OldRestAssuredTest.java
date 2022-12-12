681df084136ef292841dd07ec21190feb740f479
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-12-12 14:00:48 +0300

    spartan new base added

--- src/test/java/com/cydeo/day12/OldRestAssuredTest.java
+++ src/test/java/com/cydeo/day12/OldRestAssuredTest.java
@@ -1,4 +1,15 @@
 package com.cydeo.day12;
 
-public class OldRestAssuredTest {
+import com.cydeo.utilities.SpartanNewBase;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+public class OldRestAssuredTest extends SpartanNewBase {
+    @Test
+    public void getAllSpartan(){
+
+    }
+
+
 }
--- src/test/java/com/cydeo/utilities/SpartanNewBase.java
+++ src/test/java/com/cydeo/utilities/SpartanNewBase.java
@@ -1,15 +1,23 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
-import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
 
 public class SpartanNewBase {
     @BeforeAll
     public static void init() {
-        baseURI = "http://44.208.34.43:7000";
+
+        baseURI = "http://44.208.34.43";
+        port=7000;
+        basePath="/api";
+
     }
 
+    @AfterAll
+    public static void close(){
+        reset();
 }
 
 }
